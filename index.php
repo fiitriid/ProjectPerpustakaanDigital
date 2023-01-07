947583b657fbf0ccf90d97313d7ecb8dcb8de952
fiitriid <122152143+fiitriid@users.noreply.github.com>
2023-01-07 17:17:50 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<body bgcolor="#FFB3B3"></body>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>UTS-PDP-S1-A16</title>
+	<style>
+		body {
+			-webkit-box-sizing : border-box ;
+			-moz-box-sizing: bodrder-box ;
+			box-sizing: border-box;
+			width: 100%;
+			max-width: 960px;
+			margin: 0 auto;
+			padding: 1em;
+			font-size: 100%;
+		}
+		p {
+			font-family: var(Arial, Helvetica) sans-serif;
+			font-size: 1em;
+			line-height: 1.5;
+		}
+		a {
+			text-decoration: none;
+		}
+		a:hover {
+			border-bottom: 1px solid pink;
+		}
+		nav {
+			width: 100%;
+			height: 80px;
+			background: #FCE2AE;
+		}
+		nav ul {
+			margin: 0;
+			padding: 0;
+		}
+		nav ul li {
+			list-style-type: none;
+			display: inline-block;
+			float: left;
+			line-height: 80px;
+		}
+		nav ul li a {
+			text-decoration: overline;
+			margin: 5px;
+			padding: 14px 20px;
+			color: navy;
+			background-color: #B6FFEA;
+			border-radius: 5px;
+		}
+		nav ul li a:hover {
+			color: #fff;
+				background-color: #B6FFEA;
+		}
+		ul {
+			margin: 0;
+			padding: 0;
+			list-style: none;
+		}
+		#header ul li {
+			display: inline-block;
+			padding: .5em;
+		}
+		#content {
+			width: 70%;
+			float: left;
+		}
+		#content section {
+			margin-top: 2em;
+			border-top: 2px solid #968C83;
+		}
+		#sidebar {
+			-webkit-box-sizing: border-box;
+			-moz-box-sizing: border-box;
+			box-sizing: border-box;
+			width: 30%;
+			float: right;
+			padding-left: 1em;
+		}
+		section h3+p {
+			border-bottom: 1px solid #968C83;
+			padding-bottom: 1em;
+		}
+		section article {
+			border-bottom: 1px solid #968C83;
+			line-height: normal;
+		}
+		#footer {
+			clear: both;
+			text-align: center;
+			padding-top: 1em;
+		}
+	</style>
+</head>
+<body>
+	<nav>
+		<ul>
+			<li><a href="HALAMAN UTAMA.html">Home</a></li>
+			<li><a href="HALAMAN 2.html">Page 2</a></li>
+			<li><a href="HALAMAN 3.html">Page 3</a></li>
+		</ul>
+	</nav>
+</body>
+
+<body>
+	<h1 id="Judul">LIBRARY AND INFORMATION SCIENCE WEB</h1>
+	<header>Universitas Sumatera Utara</header>
+	<!-- halaman pembuka -->
+		<p> <!-- style="border: solid 2px #FFDCF7; background: #FFDCF7; padding: 16px; margin: 15px 0px; text-align: center; line-height: 23px; color: black; font-size: 15px; width: 500px;"> -->SELAMAT DATANG DI WEBSITE KAMI. WEBSITE INI DITUJUKAN UNTUK PEMENUHAN UJIAN TENGAH SEMESTER MATA KULIAH PERPUSTAKAAN DIGITAL PADA SEMESTER 4 TAHUN AJARAN 2022-2023</p>
+</body>
+<body>
+	<div style="background: #FFDCF7; opacity: 60%; border: 1px solid #968C83; line-height: 20px; margin: 12px; padding: 1px; text-align: center; width: 200px; font-style: italic; font-size: 13px; opacity: 90%;">
+				Anggota Kelompok :
+	<div style="text-align: left;"></div>
+		<br/>Fitri Dian Jannah - 200709049,
+		<br/>Cut Shahnaz Caesarina- 200709081, 
+		<br/>Athaya Mayori - 200709077, 
+		<br/>Puja Kusuma - 200709019, 
+		<br/>Fauzan Azima - 170709062 
+		<br/>Terimakasih </div>
+
+</body>
+<body>
+	<main id="content" class="group">
+		<!-- <aside id="sidebar" role="complementary">
+			<img src="buku_1.png" height="150px" width="150px"/>
+		</aside> -->
+	<article id="post">
+	<header>
+		<h2>Apa itu Perpustakaan?</h2>
+		<p><time datetime="2022-03-22T10:02"><i>22 Maret 2022. Pukul 10.02</i></time></p>
+	</header>
+		<p style="text-align: justify;">	Menurut UU Perpustakaan pada Bab I pasal 1 menyatakan Perpustakaan adalah institusi yang mengumpulkan pengetahuan tercetak dan terekam, mengelolanya dengan cara khusus guna memenuhi kebutuhan intelektualitas para penggunanya melalui beragam cara interaksi pengetahuan.
+		<br/>
+		<a href="https://www.bpkp.go.id/pustakabpkp/index.php?p=pengertian,%20tujuan">Perpustakaan</a> dapat juga diartikan sebagai kumpulan informasi yang bersifat ilmu pengetahuan, hiburan, rekreasi, dan ibadah yang merupakan kebutuhan hakiki manusia.
+		<br/>
+		Oleh karena itu perpustakaan modern telah didefinisikan kembali sebagai tempat untuk mengakses informasi dalam format apa pun, apakah informasi itu disimpan dalam gedung perpustakaan tersebut atau tidak. Dalam perpustakaan modern ini selain kumpulan buku tercetak, sebagian buku dan koleksinya ada dalam perpustakaan digital (dalam bentuk data yang bisa diakses lewat jaringan komputer).
+		</p>
+		<footer id="footer" role="contentinfo">
+			<p>Kunjungi Juga :</p>
+			<ul>
+				<li><a href="https://www.usu.ac.id/id">Website USU</a></li>
+				<li><a href="https://library.usu.ac.id/">Library USU</a></li>
+			</ul>
+			<img src="THX.png" height="167px" width="900px">
+		</footer>
+	</article>
+	</main>
+
+	<!-- aside pada body element -->
+	<aside id="sidebar" role="complementary">
+		<img src="buku_4.png" width="210px" height="230px">
+	</aside>
+</body>
+</html>
